1a418c8c1b160fbde9fc966e817a3044565edd7b
nikhil7620 <132567416+nikhil7620@users.noreply.github.com>
2025-06-16 22:31:22 +0530

    Second

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./Web/About";
+import Home from "./Web/Home";
+
+export default function App() {
+    return (
+            <>
+                <Home />
+                <About />    
+            </>
+       );
+  }
+  
